3d50a7fd26933f647728ad1c5090712307787d3f
hakanohi <aashutosh@tutanota.com>
2020-07-15 12:03:03 +0545

    Add blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,13 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(main_blueprint, url_prefix='/auth')
+
+    from .errors import errors as errors_blueprint
+    app.register_blueprint(errors_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
