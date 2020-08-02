c7c115be3dcb46020011bcf203c6c758dc41609e
hakanohi <aashutosh@tutanota.com>
2020-08-02 11:27:52 +0545

    Add database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 from config import config
+
+db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    db.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
--- config.py
+++ config.py
@@ -5,14 +5,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'afjlkajflkjasfkljaljladflkj')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DEV_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'TEST_DATABASE_URI') or 'sqlite://'
 
 
 config = {
@@ -20,4 +25,6 @@ config = {
     'testing': TestingConfig,
 
     'default': DevelopmentConfig
+
+
 }
