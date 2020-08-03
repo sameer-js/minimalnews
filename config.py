6d7075ad76f83a14d4f9739e63ad6375886ec91d
hakanohi <aashutosh@tutanota.com>
2020-08-03 06:58:09 +0545

    Add email configs

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'afjlkajflkjasfkljaljladflkj')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smpt.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SUBJECT_PREFIX = '[MinimalNews]'
+    MAIL_SENDER = os.environ.get('MAIL_SENDER')
 
 
 class DevelopmentConfig(Config):
