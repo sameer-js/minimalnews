604f1466be6efe70b0ae5c3383844363a1bd03ba
hakanohi <aashutosh@tutanota.com>
2020-08-23 15:28:07 +0545

    Add production configuration

--- config.py
+++ config.py
@@ -6,13 +6,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'afjlkajflkjasfkljaljladflkj')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smpt.googlemail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_SUBJECT_PREFIX = '[MinimalNews]'
-    MAIL_SENDER = os.environ.get('MAIL_SENDER')
+    MAIL_SENDER = f'Minaimal News <{os.environ.get(MAIL_SENDER)}>'
+    MINIMAL_NEWS_ADMIN = os.environ.get('MINIMAL_NEWS_ADMIN')
+    
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class DevelopmentConfig(Config):
@@ -27,9 +32,39 @@ class TestingConfig(Config):
         'TEST_DATABASE_URI') or 'sqlite://'
 
 
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+            'DATABASE_URI' or 'sqlite:///' + os.path.join(
+                basedir, 'data.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # error emails 
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.MAIL_SENDER,
+            toaddrs=[cls.MINIMAL_NEWS_ADMIN],
+            subject=f'{cls.MAIL_SUBJECT_PREFIX} Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
+    'production': ProductionConfig,
 
     'default': DevelopmentConfig
 
